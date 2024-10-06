b793a82438dcc23ce97913496caf120f3d497a66
gerazayis <gerazayisti@gmail.com>
2024-10-06 19:23:31 +0100

    update usemediaquery

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -4,14 +4,16 @@ export function useMediaQuery(query: string): boolean {
   const [matches, setMatches] = useState(false);
 
   useEffect(() => {
-    const media = window.matchMedia(query);
+    if (typeof window !== 'undefined') {
+      const media = window.matchMedia(query);
+
     if (media.matches !== matches) {
       setMatches(media.matches);
     }
     const listener = () => setMatches(media.matches);
     media.addListener(listener);
-    return () => media.removeListener(listener);
-  }, [matches, query]);
+    return () => media.removeListener(listener); }
+  }, [matches, query]); 
 
   return matches;
 }
